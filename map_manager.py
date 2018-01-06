a6dc403b5744899cd63355b3d999342355bb11d5
Connor Archard <carchard0@gmail.com>
2018-01-06 15:32:32 -0500

    switched render mode to threading and also added support for the projectile class which can break down walls

--- map_manager.py
+++ map_manager.py
@@ -1,25 +1,34 @@
-import random
 import copy
+import time
+import random
+import threading
 
 from player import Player
 
 class Map_Manager():
   # class to update the map at the start of each turn and as turns progress
-  _WALL = 0
-  _FLOOR = 1
-  _ITEM = 2
-  _PLAYER = 3
+  _WALL       = 0
+  _FLOOR      = 1
+  _ITEM       = 2
+  _PROJECTILE = 3
+  _PLAYER     = 4
 
   def __init__(self,width,height):
     # initialize the map manager with map size
+    self.mapsLock    = threading.Lock()
     self.init_map(width,height)
     self.myPlayer    = Player(xPos=4,yPos=1,gold=0)
     self.width       = width
     self.height      = height
+    with self.mapsLock:
+      self.originalMap[self.myPlayer.xPos][self.myPlayer.yPos] = self._FLOOR
+      self.currentMap  = [col[:] for col in self.originalMap]
+      self.currentMap[self.myPlayer.xPos][self.myPlayer.yPos] = self._PLAYER
 
-    self.originalMap[self.myPlayer.xPos][self.myPlayer.yPos] = self._FLOOR
-    self.currentMap  = [col[:] for col in self.originalMap]
-    self.currentMap[self.myPlayer.xPos][self.myPlayer.yPos] = self._PLAYER
+  def get_pixel(self,x,y):
+    with self.mapsLock:
+      #print 'get_pixel has lock'
+      return self.currentMap[x][y]
 
   def weighted_map_number_generator(self):
     # Returns one of the Map_Manager pre-defined constants.
@@ -34,50 +43,84 @@ class Map_Manager():
 
   def init_map(self,width=16,height=16):
     # create a map for the start of play
-    self.originalMap = []
-    for x in xrange(width):
-      self.originalMap.append([])
-      for y in xrange(height):
-        if x==0 or y==0 or x==width-1 or y==height-1:
-          self.originalMap[x].append(self._WALL)
-        else:
-          self.originalMap[x].append(self.weighted_map_number_generator())
+    with self.mapsLock:
+      print 'init_map has lock'
+      self.originalMap = []
+      for x in xrange(width):
+        self.originalMap.append([])
+        for y in xrange(height):
+          if x==0 or y==0 or x==width-1 or y==height-1:
+            self.originalMap[x].append(self._WALL)
+          else:
+            self.originalMap[x].append(self.weighted_map_number_generator())
 
   def redraw_map(self):
     self.init_map(self.width,self.height)
-    self.originalMap[self.myPlayer.xPos][self.myPlayer.yPos] = self._FLOOR
-    self.currentMap  = [col[:] for col in self.originalMap]
-    self.currentMap[self.myPlayer.xPos][self.myPlayer.yPos] = self._PLAYER
+    with self.mapsLock:
+      print 'redraw_map has lock'
+      self.originalMap[self.myPlayer.xPos][self.myPlayer.yPos] = self._FLOOR
+      self.currentMap  = [col[:] for col in self.originalMap]
+      self.currentMap[self.myPlayer.xPos][self.myPlayer.yPos] = self._PLAYER
     
   def _is_wall(self,x,y):
-    return self.currentMap[x][y] == self._WALL
-
-  def _is_item(self,x,y):
-    return self.currentMap[x][y] == self._ITEM
-
-  def _is_empty(self,x,y):
-    return self.currentMap[x][y] == self._FLOOR
-
-    if self._is_in_bounds(x,y):
-      return self.currentMap[x][y] == self._WALL
-    else:
-      return True
+      if self._is_in_bounds(x,y):
+        with self.mapsLock:
+          print '_is_wall has lock'
+          return self.currentMap[x][y] == self._WALL
+      else:
+        return True
 
   def _is_item(self,x,y):
     if self._is_in_bounds(x,y):
-      return self.currentMap[x][y] == self._ITEM
+      with self.mapsLock:
+        print '_is_item has lock'
+        return self.currentMap[x][y] == self._ITEM
     else:
       return False
 
   def _is_empty(self,x,y):
-    if self._is_in_bound(x,y):
-      return self.currentMap[x][y] == self._FLOOR
+    if self._is_in_bounds(x,y):
+      with self.mapsLock:
+        print '_is_empty has lock'
+        return self.currentMap[x][y] == self._FLOOR
     else:
       return False
 
   def _is_in_bounds(self,x,y):
     """Checks in-bound paramters."""
     return x >= 0 and y >= 0 and x < self.width and y < self.height
+
+  def track_projectile(self):
+    new_proj = self.myPlayer.shoot_projectile()
+    p_t      = threading.Thread(target=self.projectile_thread,
+                                kwargs={'proj':new_proj})
+    p_t.start()
+
+
+  def projectile_thread(self,proj):
+    while proj._is_alive():
+      print 'projectile_thread has lock'
+      old_x    = proj.get_x_position()
+      old_y    = proj.get_y_position()
+      proj.calc_next_position()
+      proj_x   = proj.get_x_position()
+      proj_y   = proj.get_y_position()
+      if self._is_wall(proj_x,proj_y):
+        proj._kill()
+        with self.mapsLock:
+          print 'projectile_thread has lock'
+          self.originalMap[proj_x][proj_y] = self._FLOOR
+          self.currentMap[proj_x][proj_y]  = self._FLOOR
+      elif self._is_item(proj_x,proj_y) or self._is_empty(proj_x,proj_y):
+        with self.mapsLock:
+          print 'projectile_thread has lock'
+          self.currentMap[proj_x][proj_y]  = self._PROJECTILE
+      with self.mapsLock:
+        print 'projectile_thread has lock'
+        self.currentMap[old_x][old_y] = self.originalMap[old_x][old_y]
+        self.currentMap[self.myPlayer.xPos][self.myPlayer.yPos] = self._PLAYER
+
+      time.sleep(proj.get_refresh_time())
 
   def move_player(self,char):
     """ Tracks the player's movement based on keypresses and prevents illegal moves.
@@ -99,12 +142,15 @@ class Map_Manager():
       self.myPlayer.set_x_position(old_x)
       self.myPlayer.set_y_position(old_y)
     elif self._is_item(new_x,new_y):
-      self.originalMap[new_x][new_y] = self._FLOOR
+      with self.mapsLock:
+        print 'move_player has lock'
+        self.originalMap[new_x][new_y] = self._FLOOR
 
       # Tracking gold acquired done here.
       self.myPlayer.acquire_gold(random.randint(1,2))
       gold_amount = self.myPlayer.get_gold_amount()
       print gold_amount
-
-    self.currentMap[old_x][old_y] = self.originalMap[old_x][old_y]
-    self.currentMap[new_x][new_y] = self._PLAYER
+    with self.mapsLock:
+      print 'move player has lock'
+      self.currentMap[old_x][old_y] = self.originalMap[old_x][old_y]
+      self.currentMap[new_x][new_y] = self._PLAYER
--- player.py
+++ player.py
@@ -1,3 +1,6 @@
+import copy
+
+from projectile import Projectile
 
 class Player():
     """Models the attributes of the playable avatar."""
@@ -5,13 +8,15 @@ class Player():
     def __init__(self, health=5, stamina=5, gold=0,
                 attack=1, defense=0, xPos=0, yPos=0):
         """Initialize player avatar attributes."""
-        self.health = health
+        self.health  = health
         self.stamina = stamina
-        self.gold = gold
-        self.attack = attack
+        self.gold    = gold
+        self.attack  = attack
         self.defense = defense
-        self.xPos = xPos
-        self.yPos = yPos
+        self.xPos    = xPos
+        self.yPos    = yPos
+        self.facing  = 'east'
+        self.attack  = Projectile(xPos,yPos,attack)
 
     def set_x_position(self,value):
         self.xPos = value
@@ -19,11 +24,17 @@ class Player():
     def set_y_position(self,value):
         self.yPos = value
 
+    def set_facing(self,value):
+        self.facing = value
+
     def get_x_position(self):
         return self.xPos
 
     def get_y_position(self):
         return self.yPos
+
+    def get_facing(self):
+        return self.facing
 
     def get_gold_amount(self):
         # Reads the current amount of gold the player has.
@@ -39,16 +50,27 @@ class Player():
         #movement = input()
         if movement == 'w':
             self.yPos -= 1
+            self.facing = 'north'
         elif movement == 'a':
             self.xPos -= 1
+            self.facing = 'west'
         elif movement == 's':
             self.yPos += 1
+            self.facing = 'south'
         elif movement == 'd':
             self.xPos += 1
+            self.facing = 'east'
         """elif movement == 'r':
             self.stamina += 1"""
 
         return ({'x':self.xPos,'y':self.yPos})
+
+    def shoot_projectile(self):
+        new_proj = copy.copy(self.attack)
+        new_proj.set_y_position(self.yPos)
+        new_proj.set_x_position(self.xPos)
+        new_proj.set_facing(self.facing)
+        return new_proj
 
 class Enemy(Player):
     def __init__(self,health=5, stamina=5, attack=1, defense=0, xPos=0, yPos=0):
